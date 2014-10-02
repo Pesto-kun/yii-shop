1d41606551bc362699d29da8271a60a81a68c15a
Pesto-kun <pest_h@mail.ru>
2014-10-02 18:21:19 +0400

    add db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_shop',
     'username' => 'root',
-    'password' => '',
+    'password' => 'myroot',
     'charset' => 'utf8',
 ];
